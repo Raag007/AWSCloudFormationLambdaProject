64becc71cfe38d4bc799715d8693c72b0f16dede
Anuraag <anurag.vodnala@gmail.com>
2020-01-31 15:31:11 -0600

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,20 @@
 import boto3
-import json
-
-ec2 = boto3.client('ec2')
-def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+def lambda_handler():
+    ec2 = boto3.client('ec2')
+    regions = ec2.describe_regions().get('Regions',[] )
+    
+    for region in regions:
+            reg=region['RegionName']
+            ec2 = boto3.client('ec2', region_name=reg)
+            result = ec2.describe_volumes( Filters=[{'Name': 'status', 'Values': ['in-use']}])
+            for volume in result['Volumes']:
+            result = ec2.create_snapshot(VolumeId=volume['VolumeId'],Description='Created by Lambda backup function ebs-snapshots')
+            ec2resource = boto3.resource('ec2', region_name=reg)
+            snapshot = ec2resource.Snapshot(result['SnapshotId'])
+            volumename = 'N/A'
+            if 'Tags' in volume:
+                for tags in volume['Tags']:
+                    if tags["Key"] == 'Name':
+                        volumename = tags["Value"]
+        
+            snapshot.create_tags(Tags=[{'Key': 'Name','Value': volumename}])
